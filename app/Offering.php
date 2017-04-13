1729a944e2eee74a1158a136a35c3b0680541b08
sorivin <sorivin2011@gmail.com>
2017-04-13 19:14:00 +0300

    Error

--- app/Offering.php
+++ app/Offering.php
@@ -4,9 +4,13 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+
 class Offering extends Model
 {
-    //
-    protected $fillable = array('offering','price');
+	protected $fillable = array('offering','price');
+    //relation to Purchase
+    public function purchases()
+    {
     return $this->hasMany('App\Purchase');
+    }
 }
